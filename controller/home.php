cc06d1f821a68aa4927a0770a1e53bb7008d1c6a
creatingdummy <Fadhil.morshidi2676@gmail.com>
2016-08-17 21:56:30 +0800

    Update home.php

--- controller/home.php
+++ controller/home.php
@@ -25,15 +25,15 @@ class Home extends Controller {
 	function NewPass($f3) {
 		$me = $this->me;
 		if ( ! Check::pass($f3->get('POST.oldpass'), $me->password)) {
-			$this->flash('Password Lama Salah');
+			$this->flash('You Have Key in wrong password');
 		} elseif ( ! Check::confirm('POST.password')) {
-			$this->flash('Konfirmasi Password Tidak Cocok');
+			$this->flash('Confirmation Password Not same');
 		} elseif ( ! $f3->exists('POST.password',$pass)) {
-			$this->flash('Password Tidak Boleh Kosong');
+			$this->flash('Password Cannot Be Empty');
 		} else {
 			$me->password = $pass;
 			$me->save();
-			$this->flash('Ganti Password Succes','success');
+			$this->flash('Change Password Succes','success');
 		}
 		$f3->reroute($f3->get('URI'));
 	}
